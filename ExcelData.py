243e5ddb126b572980b64c2778ad4c50f66bdecd
A R DANISH <helloardanish@gmail.com>
2023-07-29 14:21:26 +0300

    Update ExcelData.py

--- ExcelData.py
+++ ExcelData.py
@@ -1,9 +1,16 @@
 class ExcelData:
-    def __init__(self, date, booked, price,hotelOffice):
+    def __init__(self, date, hotelOffice, amount, exchRate, amountFC, remarks, booked):
         self._date = date
-        self._booked = booked
-        self._price = price
         self._hotelOffice=hotelOffice
+        self._amount = amount
+        self._exchRate=exchRate
+        self._amountFC=amountFC
+        self._remarks=remarks
+        self._booked = booked
+        
+        
+
+
 
     # Getter and setter for name (string type)
     @property
@@ -16,6 +23,52 @@ class ExcelData:
         print("Setting date...")
         self._date = value
 
+    
+    # Getter and setter for hotelOffice (string type)
+    @property
+    def hotelOffice(self):
+        return self._hotelOffice
+
+    @hotelOffice.setter
+    def hotelOffice(self, value):
+        self._hotelOffice = value
+
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Age must be an integer.")
+        self._amount = value
+
+    # Getter and setter for exchRate (string type)
+    @property
+    def exchRate(self):
+        return self._exchRate
+
+    @exchRate.setter
+    def exchRate(self, value):
+        self._exchRate = value
+
+
+    @property
+    def amountFC(self):
+        return self._amountFC
+
+    @amountFC.setter
+    def amountFC(self, value):
+        self._amountFC = value
+
+    @property
+    def remarks(self):
+        return self._remarks
+
+    @remarks.setter
+    def remarks(self, value):
+        self._remarks = value
 
     
     @property
@@ -26,24 +79,10 @@ class ExcelData:
     def booked(self, value):
         self._booked = value
 
-    # Getter and setter for age (integer type)
-    @property
-    def price(self):
-        return self._price
 
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Age must be an integer.")
-        self._price = value
-
-    # Getter and setter for salary (float type)
-    @property
-    def hotelOffice(self):
-        return self._hotelOffice
-
-    @hotelOffice.setter
-    def hotelOffice(self, value):
-        self._hotelOffice = value
+    
 
 #row = ExcelData("2023-07-26","YES",30,"Hotel To Office")
+#Update
+
+#row = ExcelData('Date','Paticulars(From-To)' , 'Amount', 'Exch. Rate', 'Amount(FC)','Remarks(with bill/without bill)', 'Booked')
